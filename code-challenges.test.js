994d686bf10d3e3d454589c69b12a70dda5b1cb6
lundrew <andrueee@gmail.com>
2022-04-03 16:53:58 -0700

    Completed Week 2 Assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -9,37 +9,95 @@
 
 // Reminder: The test will call your function
 // Run the file with the following command: $ yarn jest
+//_______________________________________________________________________________________________________________
 
-// --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multipled by 3.
+// // --------------------INSTRUCTOR EXAMPLE: Create a function that takes in an array of numbers and returns an array with all the numbers multipled by 3.
 
-// a) Create a test with expect statements for each of the variables provided.
+// // a) Create a test with expect statements for each of the variables provided.
 
 const numbersArray1 = [6, 7, 8, 9, 10]
 // Expected output: [18, 21, 24, 27, 30]
 const numbersArray2 = [24, 27, 30, 33, 36]
 // Expected output: [72, 81, 90, 99, 108]
 
+// Pseudo Code: 
+// start with describe
+// enter function "multBy3"
+// use "it" as a description of what we want to test
+// input the variables we are testing to see if the function works
+// input expect to have the test understand we want to use the function "multBy3" on the variables 
+
+describe("multBy3", () => {
+    it("takes in an array of numbers and returns an array with all the numbers multiplied by 3.", () => {
+      const numbersArray1 = []
+      const numbersArray2 = []
+      expect(multBy3(numbersArray1)).toEqual([])
+      expect(multBy3(numbersArray2)).toEqual([])
+    })
+  })
+
 
 // b) Create the function that makes the test pass.
+const multBy3 = (array) => {
+  return array.map(value => value * 3)
+}
 
+// OUTPUT:
+// ✓ takes in an array of numbers and returns an array with all the numbers multiplied by 3. (1 ms)
 
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
 
+// // //_________________________________________________________________________________________________________________
 // --------------------1) Create a function that takes a number as an argument and decides if the number is evenly divisble by three or not.
-
 // a) Create a test with expect statements for each of the variables provided.
 
 const num1 = 15
-// Expected output: "15 is divisible by three"
+// // Expected output: "15 is divisible by three"
 const num2 = 0
-// Expected output: "0 is divisible by three"
+// // Expected output: "0 is divisible by three"
 const num3 = -7
-// Expected output: "-7 is not divisible by three"
+// // Expected output: "-7 is not divisible by three"
+
+// Pseudo Code: 
+// start with describe
+// enter function "divisibleBy3"
+// use "it" as a description of what we want to test
+// input the variables we are testing to see if the function works
+// input expect to have the test understand we want to use the function "divisibleBy3" on the variables 
+
+describe("divisibleBy3", () => {
+    it("takes in a number as an argument and decides if the number is evenly divisible by 3 or not", () => {
+      const num1 = []
+      const num2 = []
+      const num3 = []
+      expect(divisibleBy3(num1))
+      expect(divisibleBy3(num2))
+      expect(divisibleBy3(num3))
+    })
+  })
 
 
 // b) Create the function that makes the test pass.
+// // this is the passing test
+const divisibleBy3 = (number) => {
+    if  (number % 3 === 0){
+        return "divisble by 3"
+    } else{
+      return "not divisible by 3"
+    }
+  }
+
+//OUTPUT:
+// ✓ takes in a number as an argument and decides if the number is evenly divisible by 3 or not (1 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
 
 
-
+// // _________________________________________________________________________________________________________________
 // --------------------2) Create a function that takes in an array of words and returns an array with all the words capitalized.
 
 // a) Create a test with expect statements for each of the variables provided.
@@ -49,9 +107,37 @@ const randomNouns1 = ["streetlamp", "potato", "teeth", "conclusion", "nephew"]
 const randomNouns2 = ["temperature", "database", "chopsticks", "mango", "deduction"]
 // Expected output: ["Temperature", "Database", "Chopsticks", "Mango", "Deduction"]
 
+// Pseudo Code: 
+// start with describe
+// enter function "upperCase"
+// use "it" as a description of what we want to test
+// input the variables we are testing to see if the function works
+// input expect to have the test understand we want to use the function "upperCase" on the variables 
 
-// b) Create the function that makes the test pass.
+describe("upperCase", () => {
+    it("takes in an array of words and returns an array with all the words capitalized.", () => {
+      const randomNouns1 = []
+      const randomNouns2 = []
+      expect(upperCase(randomNouns1))
+      expect(upperCase(randomNouns2))
+    })
+  })
 
+
+// // b) Create the function that makes the test pass.
+const upperCase = (string) => {
+  return string.map(value=> {
+    return value.charAt(0).toUpperCase() + value.substring(1);
+  })
+}
+
+// OUTPUT:
+// ✓ takes in an array of words and returns an array with all the words capitalized. (1 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+//_________________________________________________________________________________________________________________
 
 
 // --------------------3) Create a function that takes in a string and logs the index of the first vowel.
@@ -65,5 +151,43 @@ const vowelTester2 = "academy"
 const vowelTester3 = "challenges"
 // Expected output: 2
 
+// Pseudo Code: 
+// start with describe
+// enter function "vowelSpotter"
+// use "it" as a description of what we want to test
+// input the variables we are testing to see if the function works
+// input expect to have the test understand we want to use the function "vowelSpotter" on the variables 
+
+describe("vowelSpotter", () => {
+    it("takes in a string and logs the index of the first vowel", () => {
+      const vowelTester1 = []
+      const vowelTester2 = []
+      const vowelTester3 = []
+      expect(vowelSpotter(vowelTester1))
+      expect(vowelSpotter(vowelTester2))
+      expect(vowelSpotter(vowelTester3))
+    })
+  })
+
+
 
 // b) Create the function that makes the test pass.
+const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+const vowelSpotter = (value) => {
+  let foundIndexes = []
+  for(let i = 0; i < value.length ; i++) {
+    if (vowels.indexOf(value[i]) > -1){
+      foundIndexes.push(i);
+    }
+  } 
+  return foundIndexes.sort()[0]
+}
+
+//OUTPUT:
+// ✓ takes in a string and logs the index of the first vowel (1 ms)
+
+// Test Suites: 1 passed, 1 total
+// Tests:       1 passed, 1 total
+// Snapshots:   0 total
+// _________________________________________________________________________________________________________________
--- interview-questions.md
+++ interview-questions.md
@@ -8,49 +8,51 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is the difference between a parameter and an argument?
 
-  Your answer:
+  Your answer: The parameter is the placeholder inside a function. When the argument is passed, the argument is connected to the parameter and we can then create actions on the parameter within the function.  
 
-  Researched answer:
+  Researched answer:A parameter is the variable that is listed in the parentheses of the function, and when something is passed through the console.log (which is the argument) it is sent through that function.
 
 
 
 2. The JavaScript built in method .map() takes predefined parameters. What are they? Which are required and which are optional?
 
-  Your answer:
+  Your answer: The required parameters are the ones that need to be pushed from the argument. When an argument is passed, the map() function will alter the original array. 
 
-  Researched answer:
+  Researched answer: the array method called map(), calls a specified function for every array element and then creates a new array with that. This map() method does not change anything to the original array but creates a new array. We use this if we want to execute a different function to an array or an element within that array. 
 
 
 
 3. What is the difference between map and filter?
 
-  Your answer:
+  Your answer: Map accesses all the elements within an array and filter, filters out specific elements in an array. 
 
-  Researched answer:
+  Researched answer: The filter() array method will filter out any unwanted elements within that array. The map() array method will transform every element in an array individually. 
 
 
 
 4. What is the difference between a function and a method?
 
-  Your answer:
+  Your answer: A function is a code block that makes an action and can be used multiple times on different data. A method is a built in function that can be used within a function. 
 
-  Researched answer:
+  Researched answer: A method is a function within an object or function.  If there is no method within the object, it is just an object. If there is no object surrounding the method, then it is a function. When a function is placed within the scope of an object, it is considered a method. 
 
 
 
 5. What is object destructuring?
 
-  Your answer:
+  Your answer: Object destructuring is when you can take a property from an object and apply it to a new variable. 
 
-  Researched answer:
+  Researched answer: Destructuring is when you take the properties from an object or the indexes of an array to separate them and to then create specific variables.
+
+  object(contains properties) --> destructure(property) --> create new variable(containing same property)
 
 
 
 6. STRETCH: What is hoisting in JavaScript?
 
-  Your answer:
+  Your answer: Hoisting is related to the arrangement of code and how variables are accessed. 
 
-  Researched answer:
+  Researched answer: Hoisting is the process of moving the declaration of variables, functions or classes to the top of their scope prior to the execution of the code. It gives us the advantage that wherever the variables etc.. are declared, they are then moved to the top of their scope and does not matter if their scope is global or local. 
 
 
 
@@ -58,12 +60,17 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 Research and define the following terms to the best of your ability.
 
-1. Class Inheritance:
+1. Class Inheritance: Class inheritance is when there is a class created but we want to use that class as a template for newly created classes. This is like a parent child relationship, where the child class is inheriting traits from the parent class. You can create multiple child classes based off of the parent class. 
 
-2. React:
+2. React: React. js is an open source Javascript library that is used for building user interfaces mainly for single page applications. React also gives us the option to reuse UI components. 
 
-3. React state:
+3. React state: With React state, we can visually change the value of a variable that already has a value. 
 
-4. React lifecycle methods:
+4. React lifecycle methods: The lifecycle methods contain "Mounting lifecycle methods", "Updating lifecycle methods", "Unmounting lifecycle methods", and the "Error handling lifecycle methods". 
 
-5. DOM:
+Mounting: adding nodes to the DOM
+Updating: alters existing nodes in the DOM
+Unmounting: removes nodes from the DOM
+Error Handling: verifys that the code works and is bug free.
+
+5. DOM: DOM stands for Document Object Model. DOM is a platform interface that allows you to change the content, structure or style of a website. The DOM consists of the Document you see on a webpage, the objects that elements within the code, and the model which is the hierarchy of tags (a tree structure). 
